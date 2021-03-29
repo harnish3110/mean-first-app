83a440353e8f26971ff4c68665a06df44bd68c55
Harnish <harnish.3191@gmail.com>
2021-03-28 21:01:08 -0500

    Improving the Server file for Node

--- README.md
+++ README.md
@@ -6,13 +6,19 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
 ## Build
 
 Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
+
+## Node (Express) Server
+Run `node server.js` or `npm run start-node` which is a custom script defined in package.json file. [nodemon] package is for auto-refreshing the node server. Which is very helpful during development phase.
+
+
+## OTHER HELPFUL SCRIPTS:
+
+## Code scaffolding
+
+Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
 
 ## Running unit tests
 
@@ -25,3 +31,5 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+
--- server.js
+++ server.js
@@ -1,14 +1,63 @@
-const http = require('http');
-const app = require('./backend/app');
-const expressApp = require('./backend/app');
+const app = require("./backend/app");
+const debug = require("debug")("node-angular");
+const http = require("http");
 
-const port = process.env.PORT || 3000;
+
+// to ensure that the port value from the ENV is usable.
+const normalizePort = val => {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+};
+
+
+// CHeck which type error (if) occured and exit the server/code gracefully
+const onError = error => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+// We log that what addresses we are listening to right now.
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+  debug("Listening on " + bind);
+};
+
+const port = normalizePort(process.env.PORT || "3000");
 
 // Setting the port number for the express App
-app.set('port', port);
+app.set("port", port);
 
 //Passing the Express server code created in the ./backend/app.js file
-const server = http.createServer(expressApp);
+const server = http.createServer(app);
+server.on("error", onError);
+server.on("listening", onListening);
 
 // Either take the port number from the Environment variable or use port 3000 (Eg localhost:3000)
+// Start the server
 server.listen(port);
